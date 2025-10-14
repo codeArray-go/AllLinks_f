a6a3e83a60f69ef4ddd24fb3f93c87806431aeb4
Akhil singh Bhandari <loneranger2520@gmail.com>
2025-10-14 22:03:43 +0530

    Update themeStore.js
    Theme is stored in localstorage

--- store/themeStore.js
+++ store/themeStore.js
@@ -1,8 +1,13 @@
 import { create } from "zustand";
 
 const useThemeStore = create((set) => ({
-    isDarkMode: false,
-    toggleTheme: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
+    isDarkMode: JSON.parse(localStorage.getItem("isDarkMode")) || false,
+    toggleTheme: () =>
+        set((state) => {
+            const newTheme = !state.isDarkMode;
+            localStorage.setItem("isDarkMode", JSON.stringify(newTheme));
+            return { isDarkMode: newTheme };
+        }),
 }));
 
 export default useThemeStore;
