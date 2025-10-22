448bff47fb9a8d4ad6c7736f1afc585d03fed52d
Akhil singh Bhandari <loneranger2520@gmail.com>
2025-10-23 04:49:16 +0530

    Refactor theme store to check for window before localStorage
    Ensure localStorage access is safe by checking for window existence.

--- store/themeStore.js
+++ store/themeStore.js
@@ -1,14 +1,22 @@
-"use client"
+"use client";
 
 import { create } from "zustand";
 
 const useThemeStore = create((set) => ({
-    isDarkMode: JSON.parse(localStorage.getItem("isDarkMode")) || false,
-    toggleTheme: () =>
-        set((state) => {
-            const newTheme = !state.isDarkMode;
-            localStorage.setItem("isDarkMode", JSON.stringify(newTheme));
-            return { isDarkMode: newTheme };
-        }),
+  isDarkMode:
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("isDarkMode")) || false
+      : false,
+
+  toggleTheme: () =>
+    set((state) => {
+      const newTheme = !state.isDarkMode;
+      if (typeof window !== "undefined") {
+        localStorage.setItem("isDarkMode", JSON.stringify(newTheme));
+      }
+      return { isDarkMode: newTheme };
+    }),
 }));
+
 export default useThemeStore;
+
